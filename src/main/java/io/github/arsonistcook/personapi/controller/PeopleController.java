3229d777c7a221626434f2fab5f3240419737c0b
Arsonist-Cook <menezescleiton@gmail.com>
2021-05-16 17:48:58 -0300

    First Commit

---
+++ src/main/java/io/github/arsonistcook/personapi/controller/PeopleController.java
@@ -0,0 +1,14 @@
+package io.github.arsonistcook.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PeopleController {
+    @GetMapping
+    public String getBook(){
+        return "API Test";
+    }
+}
